75384911f3a3e29aa7d9d878caa51cf9bcbd7095
zurdosp <zurdosp@gmail.com>
2023-07-02 20:38:58 -0300

    commit incial

---
+++ kqueue/src/main/java/com/ltp/kqueue/KqueueApplication.java
@@ -0,0 +1,13 @@
+package com.ltp.kqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KqueueApplication.class, args);
+	}
+
+}
---
+++ kqueue/src/test/java/com/ltp/kqueue/KqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp.kqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
